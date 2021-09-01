fdba9de7aefb63b3a2b31d697c83b9e272546a3c
windstonp <joaor4e@gmail.com>
2021-09-01 19:56:33 -0300

    DT-03 finish delete function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -24,6 +24,7 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks => tasks.filter(element => element.id !== id));
   }
 
   return (
